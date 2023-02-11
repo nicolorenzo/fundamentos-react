ed740da0c7cbd2c37c43395d0d092867ff0fece3
nicolorenzo <nicolorenzo@live.com>
2023-02-11 14:06:44 -0300

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,11 @@
 function Home() {
-  return <h1>Lista de Presença</h1>;
+  return (
+    <>
+      <h1>Lista de Presença</h1>
+      <input type="text" placeholder="Digite o nome..." />
+      <button type="button">Adicionar</button>
+    </>
+  );
 }
 
 export default Home;
