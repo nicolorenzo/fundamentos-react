becb98ce8a6455d034ca5ecc6d5554c9523f1994
nicolorenzo <nicolorenzo@live.com>
2023-02-12 15:38:52 -0300

    Lista

---
+++ src/components/Lista/index.jsx
@@ -0,0 +1,10 @@
+import './style.css';
+
+export function Lista() {
+  return (
+    <div className="lista">
+      <strong>Nico Lorenzo</strong>
+      <small>10:10:10</small>
+    </div>
+  );
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,16 @@
 import './style.css';
 
+import { Lista } from '../../components/Lista';
+
 export function Home() {
   return (
     <div className="container">
       <h1>Lista de Presença</h1>
       <input type="text" placeholder="Digite o nome..." />
       <button type="button">Adicionar</button>
+      <Lista />
+      <Lista />
+      <Lista />
     </div>
   );
 }
